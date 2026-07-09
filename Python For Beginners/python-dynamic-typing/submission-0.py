462cab57763308d1ce5657baa1e15f19b67272db
shreyasdevraj13-cyber <shreyasdevraj13@gmail.com>
2026-07-09 12:42:29 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 7
+print(type(variable))
+variable = 7.7
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "true"
+print(type(variable))
+variable = [1, 5, 7]
+print(type(variable))
